1f78daa1ff3a810244fdfe50d8e3bbb3ad71d9fe
allchan <a11chan@hotmail.com>
2023-04-19 23:02:49 +0900

    test : User 생성 성공 및 실패 테스트 추가

---
+++ src/test/java/com/dss/realworld/user/domain/UserTest.java
@@ -0,0 +1,44 @@
+package com.dss.realworld.user.domain;
+
+import com.dss.realworld.user.User;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class UserTest {
+
+    @Test
+    void addUser() {
+        User user = User.builder()
+                .username("Jacob")
+                .password("jakejake")
+                .email("jake@jake.jake")
+                .build();
+        assertThat(user.getUsername()).isEqualTo("Jacob");
+        assertThat(user.getPassword()).isEqualTo("jakejake");
+        assertThat(user.getEmail()).isEqualTo("jake@jake.jake");
+    }
+
+    @Test
+    void failWhenUsernameIsNull() {
+        assertThatThrownBy(() -> User.builder()
+                .password("jakejake")
+                .email("jake@jake.jake")
+                .build()).isInstanceOf(IllegalArgumentException.class);
+    }
+    @Test
+    void failWhenPasswordIsNull() {
+        assertThatThrownBy(() -> User.builder()
+                .username("Jacob")
+                .email("jake@jake.jake")
+                .build()).isInstanceOf(IllegalArgumentException.class);
+    }
+    @Test
+    void failWhenEmailIsNull() {
+        assertThatThrownBy(() -> User.builder()
+                .username("Jacob")
+                .password("jakejake")
+                .build()).isInstanceOf(IllegalArgumentException.class);
+    }
+}
