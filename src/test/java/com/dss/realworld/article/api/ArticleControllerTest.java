f0c4f84b82ef2e0b5741e4b7274bfc34e200b780
allchan <a11chan@hotmail.com>
2023-04-29 17:19:21 +0900

    test : delete article controller API 테스트 작성

--- src/test/java/com/dss/realworld/article/api/ArticleControllerTest.java
+++ src/test/java/com/dss/realworld/article/api/ArticleControllerTest.java
@@ -1,6 +1,8 @@
 package com.dss.realworld.article.api;
 
 import com.dss.realworld.article.api.dto.CreateArticleRequestDto;
+import com.dss.realworld.article.app.ArticleService;
+import com.dss.realworld.article.domain.dto.GetArticleDto;
 import com.dss.realworld.article.domain.repository.ArticleRepository;
 import com.dss.realworld.user.domain.User;
 import com.dss.realworld.user.domain.repository.UserRepository;
@@ -16,6 +18,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -34,6 +37,9 @@ public class ArticleControllerTest {
 
     @Autowired
     private ArticleRepository articleRepository;
+
+    @Autowired
+    private ArticleService articleService;
 
     @BeforeEach
     void setUp() {
@@ -61,7 +67,36 @@ public class ArticleControllerTest {
     }
 
     @Test
-    void Should_Success_When_CreateArticleDtoIsNotNull() throws Exception {
+    void Should_DeleteSuccess_When_ArticleSlugAndUserIdIsValid() throws Exception {
+        //given
+        Long logonId = 1L;
+        CreateArticleRequestDto articleDto = createArticleDto();
+        GetArticleDto savedArticle = articleService.createArticle(articleDto, logonId);
+        assertThat(savedArticle.getUserId()).isEqualTo(logonId);
+
+        //when
+        String slug = savedArticle.getSlug();
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders
+                .delete("/api/articles/" + slug)
+                .contentType(MediaType.TEXT_PLAIN);
+
+        //then
+        mockMvc.perform(mockRequest).andExpect(status().isOk());
+    }
+
+    private CreateArticleRequestDto createArticleDto() {
+        CreateArticleRequestDto.CreateArticleDto createArticleDto = CreateArticleRequestDto.CreateArticleDto.builder()
+                .title("How to train your dragon")
+                .description("Ever wonder how?")
+                .body("You have to believe")
+                .build();
+
+        return new CreateArticleRequestDto(createArticleDto);
+    }
+
+    @Test
+    void Should_CreateArticleSuccess_When_CreateArticleDtoIsNotNull() throws Exception {
+        //given
         String title = "How to train your dragon";
         String description = "Ever wonder how?";
         String body = "You have to believe";
@@ -71,6 +106,7 @@ public class ArticleControllerTest {
                 .body(body)
                 .build();
 
+        //when
         CreateArticleRequestDto createArticleRequestDto = new CreateArticleRequestDto(article);
         String jsonString = objectMapper.writeValueAsString(createArticleRequestDto);
         MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders
@@ -79,6 +115,7 @@ public class ArticleControllerTest {
                 .accept(MediaType.APPLICATION_JSON)
                 .content(jsonString);
 
+        //then
         mockMvc.perform(mockRequest)
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$..title").value(title))
