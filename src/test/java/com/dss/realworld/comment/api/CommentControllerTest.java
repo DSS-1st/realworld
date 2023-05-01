2aaa5ef5ec7c95ff6ea575f755868b73e46c681c
sdongpil <pildong94@naver.com>
2023-05-02 02:48:50 +0900

    test : CommentController - Commnet 생성 테스트 작성

---
+++ src/test/java/com/dss/realworld/comment/api/CommentControllerTest.java
@@ -0,0 +1,109 @@
+package com.dss.realworld.comment.api;
+
+import com.dss.realworld.article.domain.Article;
+import com.dss.realworld.article.domain.repository.ArticleRepository;
+import com.dss.realworld.comment.domain.dto.AddCommentRequestDto;
+import com.dss.realworld.comment.domain.repository.CommentRepository;
+import com.dss.realworld.user.domain.User;
+import com.dss.realworld.user.domain.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class CommentControllerTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @BeforeEach
+    void setUp() {
+        clearTable();
+
+        User newUser = User.builder()
+                .username("Jacob000")
+                .email("jake000@jake.jake")
+                .password("jakejake")
+                .build();
+        userRepository.addUser(newUser);
+
+        Article newArticle = Article.builder()
+                .title("How to train your dragon")
+                .slug("How-to-train-your-dragon")
+                .description("Ever wonder how?")
+                .body("You have to believe")
+                .userId(newUser.getId())
+                .build();
+        articleRepository.createArticle(newArticle);
+    }
+
+    @AfterEach
+    void teatDown() {
+        clearTable();
+    }
+
+    private void clearTable() {
+        userRepository.deleteAll();
+        userRepository.resetAutoIncrement();
+
+        articleRepository.deleteAll();
+        articleRepository.resetAutoIncrement();
+
+        commentRepository.deleteAll();
+        commentRepository.resetAutoIncrement();
+    }
+
+    @Test
+    void Should_AddCommentSuccess_When_AddCommentRequestDtoAndSlugIsNotNull() throws Exception {
+        //given
+        String slug = "How-to-train-your-dragon";
+        AddCommentRequestDto addCommentRequestDto = createAddCommentRequestDto();
+
+        //when
+        String jsonString = objectMapper.writeValueAsString(addCommentRequestDto);
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders
+                .post("/api/articles/{slug}/comments",slug)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonString);
+
+        //then
+        mockMvc.perform(mockRequest)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$..id").exists())
+                .andExpect(jsonPath("$..createdAt").exists())
+                .andExpect(jsonPath("$..updatedAt").exists())
+                .andExpect(jsonPath("$..body").value("His name was my name too."));
+    }
+
+    private AddCommentRequestDto createAddCommentRequestDto() {
+        AddCommentRequestDto.AddCommentDto comment = AddCommentRequestDto.AddCommentDto.builder()
+                .body("His name was my name too.")
+                .build();
+
+        return new AddCommentRequestDto(comment);
+    }
+}
