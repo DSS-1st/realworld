6987b8f1f146636483c27def189e0bbe5af0e914
allchan <a11chan@hotmail.com>
2023-05-07 14:05:09 +0900

    refactor : final 필드 추가

--- src/main/java/com/dss/realworld/user/api/AddUserResponseDto.java
+++ src/main/java/com/dss/realworld/user/api/AddUserResponseDto.java
@@ -3,18 +3,16 @@ package com.dss.realworld.user.api;
 import com.dss.realworld.user.domain.repository.GetUserDto;
 import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @JsonRootName(value = "user")
 @Getter
-@NoArgsConstructor
 public class AddUserResponseDto {
 
-    private String email;
-    private String token;
-    private String username;
-    private String bio;
-    private String image;
+    private final String email;
+    private final String token = "";
+    private final String username;
+    private final String bio;
+    private final String image;
 
     AddUserResponseDto(GetUserDto getUserDto) {
         this.email = getUserDto.getEmail();
