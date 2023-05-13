0d23d9aee89f4853c292e2491c661937f1ec0ba0
sdongpil <pildong94@naver.com>
2023-05-13 19:44:15 +0900

    feat : ProfileController - 프로필 조회하기 작성

---
+++ src/main/java/com/dss/realworld/user/api/ProfileController.java
@@ -0,0 +1,18 @@
+package com.dss.realworld.user.api;
+
+import com.dss.realworld.user.app.ProfileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class ProfileController {
+
+    private final ProfileService profileService;
+
+    @GetMapping(value = "/profiles/{username}")
+    public GetProfileDto getProfile(@PathVariable String username) {
+       return profileService.getProfileDto(username);
+    }
+}
