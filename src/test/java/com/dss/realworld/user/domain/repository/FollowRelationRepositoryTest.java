acf207d4dbb7c532108e374a9ba39bef3fad60b3
sdongpil <pildong94@naver.com>
2023-05-14 21:47:50 +0900

    test : FollowRelationRepositoryTest - 팔로우 취소, 팔로우 체크 테스트 작성

--- src/test/java/com/dss/realworld/user/domain/repository/FollowRelationRepositoryTest.java
+++ src/test/java/com/dss/realworld/user/domain/repository/FollowRelationRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.dss.realworld.user.domain.repository;
 
 import com.dss.realworld.user.domain.FollowRelation;
+import com.dss.realworld.user.domain.User;
+import com.dss.realworld.util.UserFixtures;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -15,6 +17,9 @@ public class FollowRelationRepositoryTest {
 
     @Autowired
     private FollowRelationRepository followRelationRepository;
+    @Autowired
+    private UserRepository userRepository;
+
 
     @BeforeEach
     void setUp() {
@@ -27,6 +32,9 @@ public class FollowRelationRepositoryTest {
     }
 
     private void clearTable() {
+        userRepository.deleteAll();
+        userRepository.resetAutoIncrement();
+
         followRelationRepository.deleteAll();
         followRelationRepository.resetAutoIncrement();
     }
@@ -41,5 +49,27 @@ public class FollowRelationRepositoryTest {
         int save = followRelationRepository.save(followRelation);
 
         assertThat(save).isEqualTo(1);
+    }
+
+    @DisplayName(value = "followeeId,followerId 유효하면 팔로우 취소 성공")
+    @Test
+    void t2() {
+
+        User user1 = UserFixtures.create();
+        User user2 = User.builder()
+                .username("username")
+                .password("1234")
+                .email("@google.com")
+                .build();
+
+        userRepository.persist(user1);
+        userRepository.persist(user2);
+
+        FollowRelation followRelation = new FollowRelation(user1.getId(), user2.getId());
+        followRelationRepository.save(followRelation);
+
+        int cancelFollow = followRelationRepository.cancelFollow(user1.getId(), user2.getId());
+
+        assertThat(cancelFollow).isEqualTo(1);
     }
 }
