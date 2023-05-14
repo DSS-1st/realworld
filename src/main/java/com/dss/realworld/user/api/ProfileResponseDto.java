3280effb58bfb52b5fa597df63876adff901b1c9
sdongpil <pildong94@naver.com>
2023-05-14 18:26:26 +0900

    refactor : GetProfileDto -> ProfileResponseDto 클래스명 변경

--- src/main/java/com/dss/realworld/user/api/ProfileResponseDto.java
+++ src/main/java/com/dss/realworld/user/api/ProfileResponseDto.java
@@ -2,24 +2,27 @@ package com.dss.realworld.user.api;
 
 import com.dss.realworld.user.domain.User;
 import com.fasterxml.jackson.annotation.JsonRootName;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @JsonRootName(value = "profile")
-public class GetProfileDto {
+public class ProfileResponseDto {
 
     private final String username;
     private final String bio;
     private final String image;
-    private final boolean following = false;
+    private final boolean following ;
 
-    private GetProfileDto(String username, String bio, String image) {
+    @Builder
+    private ProfileResponseDto(String username, String bio, String image ,boolean following) {
         this.username = username;
         this.bio = bio;
         this.image = image;
+        this.following = following;
     }
 
-    public static GetProfileDto of(User user) {
-        return new GetProfileDto(user.getUsername(), user.getBio(), user.getImage());
+    public static ProfileResponseDto of(User user) {
+        return new ProfileResponseDto(user.getUsername(), user.getBio(), user.getImage() ,false);
     }
 }
