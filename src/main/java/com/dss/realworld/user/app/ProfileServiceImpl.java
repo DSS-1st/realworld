fd0e0968e1c05b8d5a4d6fea881c862dd0892f6b
sdongpil <pildong94@naver.com>
2023-05-14 18:24:26 +0900

    feat : ProfileService - followUser 작성

--- src/main/java/com/dss/realworld/user/app/ProfileServiceImpl.java
+++ src/main/java/com/dss/realworld/user/app/ProfileServiceImpl.java
@@ -1,22 +1,41 @@
 package com.dss.realworld.user.app;
 
-import com.dss.realworld.user.api.GetProfileDto;
+import com.dss.realworld.user.api.ProfileResponseDto;
+import com.dss.realworld.user.domain.FollowRelation;
 import com.dss.realworld.user.domain.User;
+import com.dss.realworld.user.domain.repository.FollowRelationRepository;
 import com.dss.realworld.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional(readOnly = true)
+//@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ProfileServiceImpl implements ProfileService {
 
     private final UserRepository userRepository;
+    private final FollowRelationRepository followRelationRepository;
 
     @Override
-    public GetProfileDto getProfileDto(String username) {
+    public ProfileResponseDto getProfileDto(String username) {
         User user = userRepository.findByUsername(username);
-        return GetProfileDto.of(user);
+        return ProfileResponseDto.of(user);
+    }
+
+    @Override
+    public ProfileResponseDto followUser(String username,Long followerId) {
+        User followUser = userRepository.findByUsername(username);
+        FollowRelation followRelation = new FollowRelation(followUser.getId(), followerId);
+        followRelationRepository.save(followRelation);
+
+        ProfileResponseDto profileResponseDto = ProfileResponseDto.builder()
+                .username(followUser.getUsername())
+                .bio(followUser.getBio())
+                .image(followUser.getImage())
+                .following(true)
+                .build();
+
+        return profileResponseDto;
     }
 }
