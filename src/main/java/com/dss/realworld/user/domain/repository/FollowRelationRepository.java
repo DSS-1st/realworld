57b8582453612f2234f20ea6349b5c9c947dd476
sdongpil <pildong94@naver.com>
2023-05-14 21:47:26 +0900

    feat : FollowRelationRepository - 팔로우 취소, 팔로우 체크 작성

--- src/main/java/com/dss/realworld/user/domain/repository/FollowRelationRepository.java
+++ src/main/java/com/dss/realworld/user/domain/repository/FollowRelationRepository.java
@@ -7,6 +7,10 @@ import org.apache.ibatis.annotations.Mapper;
 public interface FollowRelationRepository {
     int save(FollowRelation followRelation);
 
+    int cancelFollow (Long followeeId, Long followerId);
+
+    int followCheck(Long followeeId, Long followerId);
+
     void deleteAll();
 
     void resetAutoIncrement();
