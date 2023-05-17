fd2aae36d4b3f2340beb182005435d51cea62e27
sdongpil <pildong94@naver.com>
2023-05-17 21:54:16 +0900

    fix : 주석 추가

--- src/main/java/com/dss/realworld/user/api/ProfileController.java
+++ src/main/java/com/dss/realworld/user/api/ProfileController.java
@@ -16,6 +16,7 @@ public class ProfileController {
        return profileService.get(username);
     }
 
+    //14번 브랜치에서 수정했음
     @PostMapping(value = "/profiles/{username}/follow")
     public ProfileResponseDto followUser(@PathVariable String username, @RequestParam Long followerId) {
        return profileService.follow(username,followerId);
