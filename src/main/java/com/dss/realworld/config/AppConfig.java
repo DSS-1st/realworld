64c0e1c1a145811b86d519893df46612dbd73d01
sdongpil <pildong94@naver.com>
2023-05-17 21:56:56 +0900

    refactor : 코드 정리

--- src/main/java/com/dss/realworld/config/AppConfig.java
+++ src/main/java/com/dss/realworld/config/AppConfig.java
@@ -1,7 +1,6 @@
 package com.dss.realworld.config;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
