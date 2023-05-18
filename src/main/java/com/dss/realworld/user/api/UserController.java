dc4e96ec109a7dffcb3cca9bc2c310432ce6ea91
sdongpil <pildong94@naver.com>
2023-05-18 19:07:14 +0900

    feat : UserController - update 작성

--- src/main/java/com/dss/realworld/user/api/UserController.java
+++ src/main/java/com/dss/realworld/user/api/UserController.java
@@ -3,10 +3,7 @@ package com.dss.realworld.user.api;
 import com.dss.realworld.user.app.UserService;
 import com.dss.realworld.user.domain.User;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping(value = "/api")
 @RestController
@@ -20,5 +17,16 @@ public class UserController {
         User user = userService.save(addUserRequestDto);
 
         return new AddUserResponseDto(user);
+    }
+
+    @PutMapping(value = "/users")
+    public AddUserResponseDto update(@RequestBody UpdateUserRequestDto updateUserRequestDto) {
+        User user = userService.update(updateUserRequestDto,getLoginUserId());
+
+        return new AddUserResponseDto(user);
+    }
+
+    private Long getLoginUserId() {
+        return 1L;
     }
 }
