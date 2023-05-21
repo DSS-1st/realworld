3e67db4a10687512918e0f857514f08f5a7646c1
allchan <a11chan@hotmail.com>
2023-05-21 21:15:19 +0900

    test : ArticleTag Repository 테스트 코드 작성

--- src/main/java/com/dss/realworld/article/domain/repository/ArticleTagRepository.java
+++ src/main/java/com/dss/realworld/article/domain/repository/ArticleTagRepository.java
@@ -3,18 +3,16 @@ package com.dss.realworld.article.domain.repository;
 import com.dss.realworld.article.domain.ArticleTag;
 import org.apache.ibatis.annotations.Mapper;
 
-import java.util.List;
-
 @Mapper
 public interface ArticleTagRepository {
 
     void persist(ArticleTag articleTag);
 
-    List<ArticleTag> findById(Long articleTagId);
+    ArticleTag findById(Long articleTagId);
 
     int update(ArticleTag articleTag);
 
     int delete(Long id);
 
-    int deleteByArticle(Long id);
+    int deleteByArticle(Long articleId);
 }
--- src/test/java/com/dss/realworld/article/domain/repository/ArticleTagRepositoryTest.java
+++ src/test/java/com/dss/realworld/article/domain/repository/ArticleTagRepositoryTest.java
@@ -12,14 +12,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ActiveProfiles(value = "test")
-@Sql(value = {"classpath:db/ArticleTeardown.sql","classpath:db/UserTeardown.sql"})
+//@ActiveProfiles(value = "test")
+@Sql(value = {"classpath:db/ArticleTeardown.sql","classpath:db/UserTeardown.sql","classpath:db/TagTeardown.sql","classpath:db/ArticleTagTeardown.sql"})
 @SpringBootTest
 @Transactional
 public class ArticleTagRepositoryTest {
@@ -56,5 +55,74 @@ public class ArticleTagRepositoryTest {
 
         //then
         assertThat(articleTag.getId()).isNotNull();
+    }
+
+    @DisplayName(value = "articleTagId로 조회 성공")
+    @Test
+    void t2() {
+        //given
+        User user = UserFixtures.create();
+        userRepository.persist(user);
+
+        Article article = ArticleFixtures.of(user.getId());
+        articleRepository.persist(article);
+
+        Tag tag = new Tag("qwerty");
+        tagRepository.persist(tag);
+
+        ArticleTag articleTag = new ArticleTag(article.getId(), tag.getId());
+        articleTagRepository.persist(articleTag);
+
+        //when
+        ArticleTag foundArticleTag = articleTagRepository.findById(articleTag.getId());
+
+        //then
+        assertThat(foundArticleTag.getId()).isEqualTo(articleTag.getId());
+    }
+
+    @DisplayName(value = "articleTagId로 delete 성공")
+    @Test
+    void t3() {
+        //given
+        User user = UserFixtures.create();
+        userRepository.persist(user);
+
+        Article article = ArticleFixtures.of(user.getId());
+        articleRepository.persist(article);
+
+        Tag tag = new Tag("qwerty");
+        tagRepository.persist(tag);
+
+        ArticleTag articleTag = new ArticleTag(article.getId(), tag.getId());
+        articleTagRepository.persist(articleTag);
+
+        //when
+        int result = articleTagRepository.delete(tag.getId());
+
+        //then
+        assertThat(result).isEqualTo(1);
+    }
+
+    @DisplayName(value = "articleId로 delete 성공")
+    @Test
+    void t4() {
+        //given
+        User user = UserFixtures.create();
+        userRepository.persist(user);
+
+        Article article = ArticleFixtures.of(user.getId());
+        articleRepository.persist(article);
+
+        Tag tag = new Tag("qwerty");
+        tagRepository.persist(tag);
+
+        ArticleTag articleTag = new ArticleTag(article.getId(), tag.getId());
+        articleTagRepository.persist(articleTag);
+
+        //when
+        int result = articleTagRepository.deleteByArticle(article.getId());
+
+        //then
+        assertThat(result).isEqualTo(1);
     }
 }
