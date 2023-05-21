91363775bd67ec4090b627a5ebe09294df645c51
allchan <a11chan@hotmail.com>
2023-05-21 15:16:59 +0900

    test : test container 적용

--- src/test/java/com/dss/realworld/tag/api/TagControllerTest.java
+++ src/test/java/com/dss/realworld/tag/api/TagControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -17,6 +18,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ActiveProfiles(value = "test")
 @Sql(value = "classpath:db/TagTeardown.sql")
 @SpringBootTest
 @AutoConfigureMockMvc
