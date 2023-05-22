f77a2dfb4e1b39a2a3ecb29afd97fc650d2a8f56
allchan <a11chan@hotmail.com>
2023-05-22 22:55:55 +0900

    fix : 테스트 데이터 수정

--- src/main/resources/db/dataSetup.sql
+++ src/main/resources/db/dataSetup.sql
@@ -2,7 +2,7 @@ insert into users(email, password, username)
 values ('jake000@jake.jake', 'jakejake', 'Jacob000');
 
 insert into users(email, password, username)
-values ('newuser@realworld.com', 'newpwd', 'newname');
+values ('kate@realworld.com', 'katekate', 'kate');
 
 insert into article(title, slug, body, description, user_id)
 values ('new title 1', 'new-title-1', 'body content', 'article description', 1);
