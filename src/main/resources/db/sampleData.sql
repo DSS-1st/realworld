007b664c78bc829d46e9ca2d57a4cb4e34671106
allchan <a11chan@hotmail.com>
2023-05-22 17:47:07 +0900

    test : 테스트 데이터 초기화 sql 수정

--- src/main/resources/db/sampleData.sql
+++ src/main/resources/db/sampleData.sql
@@ -1,5 +1,8 @@
 insert into users(email, password, username)
-values ('user1@realworld.com', 'user1', 'user1234');
+values ('jake000@jake.jake', 'jakejake', 'Jacob000');
+
+insert into users(email, password, username)
+values ('newuser@realworld.com', 'newpwd', 'newname');
 
 insert into article(title, slug, body, description, user_id)
 values ('new title 1', 'new-title-1', 'body content', 'article description', 1);
