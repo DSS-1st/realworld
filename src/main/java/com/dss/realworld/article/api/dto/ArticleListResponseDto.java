e36de9f8e663708bb2b82e8cc40b9f7f7af4a6fb
allchan <a11chan@hotmail.com>
2023-05-28 20:09:21 +0900

    feat : Feed Articles 구현 및 코드 정리

---
+++ src/main/java/com/dss/realworld/article/api/dto/ArticleListResponseDto.java
@@ -0,0 +1,24 @@
+package com.dss.realworld.article.api.dto;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class ArticleListResponseDto {
+
+    private final List<ArticleResponseDto> articles;
+
+    private final int articlesCount;
+
+    public ArticleListResponseDto(final List<ArticleResponseDto> articles) {
+        this.articles = articles;
+        this.articlesCount = articles.size();
+    }
+
+    public ArticleListResponseDto(final int articlesCount) {
+        this.articles = new ArrayList<>();
+        this.articlesCount = articlesCount;
+    }
+}
--- src/main/java/com/dss/realworld/common/dto/AuthorDto.java
+++ src/main/java/com/dss/realworld/common/dto/AuthorDto.java
@@ -8,15 +8,16 @@ public class AuthorDto {
     private final String username;
     private final String bio;
     private final String image;
-    private final boolean following = false;
+    private final boolean following;
 
-    private AuthorDto(String username, String bio, String image) {
+    private AuthorDto(String username, String bio, String image, boolean followed) {
         this.username = username;
         this.bio = bio;
         this.image = image;
+        this.following = followed;
     }
 
-    public static AuthorDto of(String username, String bio, String image) {
-        return new AuthorDto(username, bio, image);
+    public static AuthorDto of(String username, String bio, String image, boolean followed) {
+        return new AuthorDto(username, bio, image, followed);
     }
 }
