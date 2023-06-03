954894acbc01de726f3a111dd9503de47ba9cb94
allchan <a11chan@hotmail.com>
2023-06-03 16:51:28 +0900

    refactor : 요청 객체 DTO 유효성 검사 예외 클래스 AOP 적용

---
+++ src/main/java/com/dss/realworld/common/aop/CustomValidationAdvice.java
@@ -0,0 +1,41 @@
+package com.dss.realworld.common.aop;
+
+import com.dss.realworld.common.error.exception.CustomValidationException;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Aspect
+@Component
+public class CustomValidationAdvice {
+
+    @Pointcut(value = "@annotation(org.springframework.web.bind.annotation.PostMapping)")
+    public void postMapping() {
+    }
+
+    @Pointcut(value = "@annotation(org.springframework.web.bind.annotation.PutMapping)")
+    public void putMapping() {
+    }
+
+    @Around(value = "postMapping() || putMapping()")
+    public Object validateAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        for (Object arg : proceedingJoinPoint.getArgs()) checkOrThrow(arg);
+
+        return proceedingJoinPoint.proceed();
+    }
+
+    private void checkOrThrow(final Object arg) {
+        if (arg instanceof BindingResult && ((BindingResult) arg).hasErrors()) {
+            List<String> messageList = ((BindingResult) arg).getFieldErrors().stream()
+                    .map(fieldError -> fieldError.getField() + " : " + fieldError.getDefaultMessage())
+                    .collect(Collectors.toList());
+            throw new CustomValidationException(messageList);
+        }
+    }
+}
