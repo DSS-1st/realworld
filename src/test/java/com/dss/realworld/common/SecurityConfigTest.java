7cf8f5be076715aa1792d92cdfe40b93502ee85e
allchan <a11chan@hotmail.com>
2023-06-06 09:51:59 +0900

    test : 인증 처리 테스트 작성

---
+++ src/test/java/com/dss/realworld/common/SecurityConfigTest.java
@@ -0,0 +1,34 @@
+package com.dss.realworld.common;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+public class SecurityConfigTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @DisplayName(value = "사용자 인증 없이 인증이 필요한 주소로 접근 시 에러 메시지 반환")
+    @Test
+    void authentication_test() throws Exception {
+        //given
+        //when
+        //then
+        mockMvc.perform(post("/api/articles"))
+                .andDo(print())
+                .andExpect(jsonPath("$.statusCode").value(401))
+                .andExpect(jsonPath("$.statusReason").value("Unauthorized"))
+                .andExpect(jsonPath("$.errors.body[0]").value("사용자 인증이 필요합니다."));
+    }
+}
