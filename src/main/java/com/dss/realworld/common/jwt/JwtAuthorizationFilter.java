57cfe6d5e59c5faa26c54cd609687000bed38595
allchan <a11chan@hotmail.com>
2023-06-06 23:40:00 +0900

    refactor : 작동 확인을 위한 Logger 추가

--- src/main/java/com/dss/realworld/common/jwt/JwtAuthorizationFilter.java
+++ src/main/java/com/dss/realworld/common/jwt/JwtAuthorizationFilter.java
@@ -1,6 +1,8 @@
 package com.dss.realworld.common.jwt;
 
 import com.dss.realworld.common.auth.LoginUser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,12 +18,16 @@ import java.util.Collections;
 //토큰 검증 로직(모든 주소에서 동작)
 public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
 
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     public JwtAuthorizationFilter(AuthenticationManager authenticationManager) {
         super(authenticationManager);
     }
 
     @Override
     protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response, final FilterChain chain) throws IOException, ServletException {
+        log.debug("디버그: doFilterInternal() 호출 완료");
+
         if (isHeaderVerified(request, response)) {
             String token = request.getHeader(JwtVO.HEADER).replace(JwtVO.TOKEN_PREFIX, "");
             LoginUser loginUser = JwtProcess.verify(token);
