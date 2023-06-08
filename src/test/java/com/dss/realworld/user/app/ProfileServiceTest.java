b319728f4bef249fbe704a9e1a3b972a0e266335
allchan <a11chan@hotmail.com>
2023-06-08 16:04:07 +0900

    refactor : Profile Service 계층 테스트 스프링 시큐리티 반영

--- src/test/java/com/dss/realworld/user/app/ProfileServiceTest.java
+++ src/test/java/com/dss/realworld/user/app/ProfileServiceTest.java
@@ -1,10 +1,15 @@
 package com.dss.realworld.user.app;
 
+import com.dss.realworld.common.error.exception.UserNotFoundException;
 import com.dss.realworld.user.api.dto.ProfileResponseDto;
+import com.dss.realworld.user.domain.User;
+import com.dss.realworld.user.domain.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 
@@ -18,22 +23,36 @@ public class ProfileServiceTest {
     @Autowired
     private ProfileService profileService;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @DisplayName(value = "username이 유효하면 GetProfileDto 가져오기 성공")
+    @WithUserDetails(value = "jake@jake.jake")
     @Test
     void t1() {
-        ProfileResponseDto profileDto = profileService.get("Jacob", 1L);
+        User foundUser = getLoginUser();
+
+        ProfileResponseDto profileDto = profileService.get("Jacob", foundUser.getId());
 
         assertThat(profileDto.getUsername()).isEqualTo("Jacob");
     }
 
+    private User getLoginUser() {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        User foundUser = userRepository.findByEmail(email).orElseThrow(UserNotFoundException::new);
+        return foundUser;
+    }
+
     @DisplayName(value = "팔로우 대상이 유효하면 팔로우 성공 ")
+    @WithUserDetails(value = "kate@realworld.com")
     @Test
     void t2() {
         //given
         String targetName = "Jacob";
+        User foundUser = getLoginUser();
 
         //when
-        ProfileResponseDto profileResponseDto = profileService.follow(targetName, 2L);
+        ProfileResponseDto profileResponseDto = profileService.follow(targetName, foundUser.getId());
 
         //then
         assertThat(profileResponseDto.getUsername()).isEqualTo(targetName);
@@ -41,14 +60,16 @@ public class ProfileServiceTest {
     }
 
     @DisplayName("username, toUserId 유효하면 팔로우 취소")
+    @WithUserDetails(value = "kate@realworld.com")
     @Test
     void t3() {
         //given
         String targetName = "Jacob";
+        User foundUser = getLoginUser();
 
         //when
         profileService.follow(targetName, 2L);
-        ProfileResponseDto profileResponseDto = profileService.unFollow(targetName, 2L);
+        ProfileResponseDto profileResponseDto = profileService.unFollow(targetName, foundUser.getId());
 
         //then
         assertThat(profileResponseDto.getUsername()).isEqualTo(targetName);
