1f8b17b511ff869dd7f6c4b6751199623fae1277
allchan <a11chan@hotmail.com>
2023-06-08 15:51:52 +0900

    refactor : 테스트 편의를 위한 환경설정 수정

--- src/test/java/com/dss/realworld/common/JwtAuthorizationFilterTest.java
+++ src/test/java/com/dss/realworld/common/JwtAuthorizationFilterTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -17,7 +16,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-@ActiveProfiles(value = "test")
 @Sql(value = {"classpath:db/teardown.sql", "classpath:db/dataSetup.sql"})
 @AutoConfigureMockMvc
 @SpringBootTest
